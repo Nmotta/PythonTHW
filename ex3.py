42e179f302ce076d36f0c5254f9945bb63be0580
Nicholas Motta <41840720+Nmotta@users.noreply.github.com>
2019-09-07 21:30:40 -0500

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,30 @@
+# prints sting
+print "I will now count my chickens:"
+
+# prints Hens & Roosters and calculates number
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints string
+print "Now I will count the eggs:"
+
+# prints result of calc of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# print and calc
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# calc and returns statement as boolean expression
+print 3 + 2 < 5 - 7
+
+# eval and return integer
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
